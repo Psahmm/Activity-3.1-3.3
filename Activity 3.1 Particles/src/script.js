5e1a6a1a927b52f96cfab8c420d0285cece6e80d
Psahmm Jonio <psahmmmjonioo@gmail.com>
2025-11-02 16:52:26 +0800

    Add Activities 3.1 to 3.3 – Three.js Projects

---
+++ Activity 3.1 Particles/src/script.js
@@ -0,0 +1,106 @@
+import * as THREE from 'three'
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js'
+import * as dat from 'lil-gui'
+
+/**
+ * Debug
+ */
+const gui = new dat.GUI()
+
+/**
+ * Canvas
+ */
+const canvas = document.querySelector('canvas.webgl')
+
+/**
+ * Scene
+ */
+const scene = new THREE.Scene()
+
+/**
+ * Particles
+ */
+const particlesGeometry = new THREE.BufferGeometry()
+const count = 5000
+const positions = new Float32Array(count * 3)
+const colors = new Float32Array(count * 3)
+
+for (let i = 0; i < count * 3; i++) {
+  positions[i] = (Math.random() - 0.5) * 10
+  colors[i] = Math.random()
+}
+
+particlesGeometry.setAttribute('position', new THREE.BufferAttribute(positions, 3))
+particlesGeometry.setAttribute('color', new THREE.BufferAttribute(colors, 3))
+
+const particlesMaterial = new THREE.PointsMaterial({
+  size: 0.05,
+  sizeAttenuation: true,
+  vertexColors: true,
+  transparent: true,
+  blending: THREE.AdditiveBlending
+})
+
+const particles = new THREE.Points(particlesGeometry, particlesMaterial)
+scene.add(particles)
+
+/**
+ * Sizes
+ */
+const sizes = {
+  width: window.innerWidth,
+  height: window.innerHeight
+}
+
+window.addEventListener('resize', () => {
+  sizes.width = window.innerWidth
+  sizes.height = window.innerHeight
+  camera.aspect = sizes.width / sizes.height
+  camera.updateProjectionMatrix()
+  renderer.setSize(sizes.width, sizes.height)
+  renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
+})
+
+/**
+ * Camera
+ */
+const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height, 0.1, 100)
+camera.position.z = 3
+scene.add(camera)
+
+/**
+ * Controls
+ */
+const controls = new OrbitControls(camera, canvas)
+controls.enableDamping = true
+
+/**
+ * Renderer
+ */
+const renderer = new THREE.WebGLRenderer({ canvas })
+renderer.setSize(sizes.width, sizes.height)
+renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
+
+/**
+ * Animate
+ */
+const clock = new THREE.Clock()
+
+const tick = () => {
+  const elapsedTime = clock.getElapsedTime()
+
+  // Animate particles (wave motion)
+  const positions = particlesGeometry.attributes.position.array
+  for (let i = 0; i < count; i++) {
+    const i3 = i * 3
+    const x = positions[i3]
+    positions[i3 + 1] = Math.sin(elapsedTime + x)
+  }
+  particlesGeometry.attributes.position.needsUpdate = true
+
+  controls.update()
+  renderer.render(scene, camera)
+  window.requestAnimationFrame(tick)
+}
+
+tick()
